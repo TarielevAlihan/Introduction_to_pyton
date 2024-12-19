a59622cc2ed7bed9f2ee41944c146e49548383c3
TarielevAlihan <tarielevalihan@gmail.com>
2024-12-19 14:41:19 +0600

    commit

--- puton/lesonnumber1.py
+++ puton/lesonnumber1.py
@@ -11,3 +11,42 @@
 #print =('Hi',name + " is " + age + " years old" ,'and your height is', height)
 
 
+
+#myName = ('Jack')
+#print(myName)
+
+
+# myName = (input('Enter your name: '))
+# myName = (input('Enter your name '))
+# print(myName)
+
+
+
+# name = input("What is your name? ")
+# surName = input("What is your surname? ")
+# print("Hello ", name + surName)
+
+
+
+# celsus = 7
+# fahrenheit = 9/5 * celsus + 32
+# print(fahrenheit)
+
+#
+# radius = 15
+# area = 3.14 * radius * radius
+# print(area)
+# radius1 = area
+
+num1 = 10
+num2 = 8
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+
+
