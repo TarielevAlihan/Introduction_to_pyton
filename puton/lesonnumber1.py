be8fa3323e2e4f88e20450fbd65c22d80c76ad59
TarielevAlihan <tarielevalihan@gmail.com>
2024-12-19 13:46:26 +0600

    second commit

--- puton/lesonnumber1.py
+++ puton/lesonnumber1.py
@@ -1,1 +1,11 @@
-print('Hello World')
+#print('2*2=',2*2)
+#print('2*3=',2*3)
+#print('2*4=',2*4)
+#print('2*5=',2*5)
+#print('2*6=',2*6)
+
+
+#name = input("What is your name? ")
+#age = input("How old are you? ")
+#height = input("How tall are you? ")
+#print =('Hi',name + " is " + age + " years old" ,'and your height is', height)
